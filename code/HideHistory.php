c5dfdcc60561d6b04a0bd188ce437209a6b511ce
helpfulrobot <chris@silverstripe.com>
2016-01-01 03:44:53 +1300

    Converted to PSR-2

--- code/HideHistory.php
+++ code/HideHistory.php
@@ -6,14 +6,15 @@
  * @author Gabriele Brosulo <gabriele.brosulo@zirak.it>
  * @creation-date 23-Oct-2014
  */
-class HideHistory extends DataExtension {
-	
-	public function updateCMSFields(\FieldList $fields) {
-		
-		if (!Permission::check('ADMIN')) {
-			Requirements::css(HIDE_HISTORY_BASE . '/css/hide-history.css');
-		}
-		
-		parent::updateCMSFields($fields);
-	}
+class HideHistory extends DataExtension
+{
+    
+    public function updateCMSFields(\FieldList $fields)
+    {
+        if (!Permission::check('ADMIN')) {
+            Requirements::css(HIDE_HISTORY_BASE . '/css/hide-history.css');
+        }
+        
+        parent::updateCMSFields($fields);
+    }
 }
